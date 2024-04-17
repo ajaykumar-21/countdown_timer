697432f5b75d8e7068394b8a61f9088b0941aba7
ajaykumar-21 <ajaykumarpk789@gmail.com>
2024-04-17 16:13:14 +0530

    finish coundown timer

---
+++ timer/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import HomePage from "./components/Homepage/Homepage";
+
+function App() {
+  return (
+    <div className="app-container">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
